95205b58a93eee789b2d072b735298e764f3ed1c
faureunige <juliette.faure@etu.unige.ch>
2021-11-17 12:40:38 +0100

    Analysis

--- JULIETTE/Marketing_analysis.R
+++ JULIETTE/Marketing_analysis.R
@@ -2,3 +2,8 @@
 data <- read.table("marketing_campaign.csv", header=T, sep="\t")
 View(data)
 
+data$age <- 2021 - data$Year_Birth
+summary(data)
+as.factor(data$Marital_Status)
+which(data$Marital_Status=="absurd")
+mean(data$age)
