f34ae85dd3494a1b48c80cbd1f36f01f2b207bb6
danielsgranja <daniel.s.granja@outlook.com>
2021-11-24 16:45:56 +0100

    Characterisation of the variables

---
+++ DANIEL/Introduction.R
@@ -0,0 +1,31 @@
+Raw_data <- read.csv2("C:/Users/dsg96/OneDrive - unige.ch/Uni/Neuro M2 & Psycho M1/Automne/SP - Statistics and Probability/DAP_2021/DAP_2021/DANIEL/Raw_data.csv")
+
+#characterization of the dataset and useful variables
+#This dataset comes from an experiment that aimed to study the impact of emotion elicitation through words on associative memory.
+#For this project, we will focus on the affective part of the dataset. 
+#for that, we will keep only some variables that are determinant for this analysis:
+#Independent variables: the subjects, the wordpairs, the emotion elicited and the sex
+#Dependent variables: the disgust and the fear (Likert scales)
+
+#Creating a working dataset
+#Giving nice names to variables
+subjects<-Raw_data$ï..code
+wordpairs<-Raw_data$wordpair
+emotion<-Raw_data$emotion
+gender<-Raw_data$sex
+disgust<-Raw_data$disgust
+fear<-Raw_data$fear
+
+#Creating a dataset
+Data<-data.frame(subjects,wordpairs,emotion,gender,disgust,fear)
+
+#Adapting the variables
+Data$emotion<-as.factor(Data$emotion)
+Data$gender<-as.factor(Data$gender)
+
+#Description of the variables
+#The "subjects" and the "wordpairs" variables are characters
+#The "emotion" and the "gender" variables are factors
+#The "disgust" and the "fear" variables are integers
+
+#the
