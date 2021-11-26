334a66e9e51a0108255bd1fe2ab9cdbe2b7eb7b4
danielsgranja <daniel.s.granja@outlook.com>
2021-11-26 01:30:58 +0100

    Descrition of the variable codes

--- DANIEL/Introduction.R
+++ DANIEL/Introduction.R
@@ -1,6 +1,6 @@
 Raw_data <- read.csv2("C:/Users/dsg96/OneDrive - unige.ch/Uni/Neuro M2 & Psycho M1/Automne/SP - Statistics and Probability/DAP_2021/DAP_2021/DANIEL/Raw_data.csv")
 
-#characterization of the dataset and useful variables
+#Characterization of the dataset and useful variables
 #This dataset comes from an experiment that aimed to study the impact of emotion elicitation through words on associative memory.
 #For this project, we will focus on the affective part of the dataset. 
 #for that, we will keep only some variables that are determinant for this analysis:
@@ -28,4 +28,6 @@ Data$gender<-as.factor(Data$gender)
 #The "emotion" and the "gender" variables are factors
 #The "disgust" and the "fear" variables are integers
 
-#the
+#The emotion factors are coded as 1=disgust, 2=fear and 3=neutral
+#The gender factors are coded as m=man and f=woman
+#The disgust and the fear variables are coded form 1 to 7, 1 being feeling "not at all" the emotion and 7 being feeling "very much" the emotion
