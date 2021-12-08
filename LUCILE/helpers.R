bbe4399c60e16fbadacdd3981699e34381557e88
munoztd0 <david.munoztord@mailbox.org>
2021-12-08 14:46:40 +0100

    main

--- LUCILE/helpers.R
+++ LUCILE/helpers.R
@@ -1,5 +1,5 @@
-#for diag plot glm
-
-library(boot)
-diag <- glm.diag(model)
-glm.diag.plots(model, diag)
+  #for diag plot glm
+  
+  library(boot)
+  diag <- glm.diag(model)
+  glm.diag.plots(model, diag)
